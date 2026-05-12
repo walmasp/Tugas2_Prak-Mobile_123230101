08d0646876138e81e3d8ccaa5e505174b7e599b8
Alma Wulan <150516392+walmasp@users.noreply.github.com>
2026-05-12 23:14:12 +0700

    Update cart_binding.dart
    Removed unnecessary comment about controller structure.

--- lib/bindings/cart_binding.dart
+++ lib/bindings/cart_binding.dart
@@ -4,7 +4,6 @@ import '../controllers/cart_controller.dart';
 class CartBinding extends Bindings {
   @override
   void dependencies() {
-    // Controller sudah di-put di awal, tapi ini memenuhi syarat struktur Binding
     Get.lazyPut(() => CartController());
   }
 }
